5941d416c6e71a2e84cc178c381aac3e6496e58c
Shahinur04 <shahinur04042002@gmail.com>
2023-03-19 09:10:48 +0600

    condition

---
+++ js/condition.js
@@ -0,0 +1,10 @@
+//mathematical operator
+//{ ==, !==, >, <, <=, >= }
+
+
+if (i===100) {
+    console.log("equal to 100")
+}
+else{
+    console.log("not equal to 100")
+}
---
+++ js/variable.js
@@ -0,0 +1,14 @@
+//how to declare a variable
+// there are two types of variable ---global and local
+//we will use only let and const variable 
+//let for changing declaration 
+//const is constant 
+
+const jannat = 'x,y';
+const nishat = 19;
+
+console.log(jannat,nishat);
+
+let variable = 21;
+variable=22;
+console.log(variable);
